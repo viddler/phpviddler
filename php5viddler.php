239a5956b50ce533c74a07b40966a2dd6fbc394d
Kyle Slattery <kyle@kyleslattery.com>
2008-07-31 15:54:47 -0400

    Adding Phpviddler files

---
+++ php5viddler.php
@@ -0,0 +1,49 @@
+<?php
+include_once('phpviddler.php');
+// uses('RequestCache');
+loadModels();
+$GLOBALS['total_requests'] = 0;
+$GLOBALS['total_request_time'] = 0;
+
+class Php5viddler extends Phpviddler {
+  var $requireCache = false;
+  
+  function sendRequest($method=null,$args=null,$postmethod='get',$tryagain=true) {    
+    $result = parent::sendRequest($method, $args, $postmethod);
+    
+    if($tryagain && is_null($result)) {
+      $result = parent::sendRequest($method, $args, $postmethod, false);
+    } elseif(is_null($result)) {
+      throw new ViddlerException("No response", $method, 8888, 'n/a');
+    }
+    
+    if(is_array($result) && $result['error']) {
+      throw new ViddlerException($result['error']['description'], $method, $result['error']['code'], $result['error']['details']);
+    }
+
+		return $result;
+  }
+
+}
+
+class ViddlerException extends Exception {
+  var $details;
+  var $method;
+  
+  public function __construct($message, $method, $code=0, $details='') {
+    $this->details = $details;
+    $this->method = $method;
+    parent::__construct($message, $code);
+  }
+  
+  public function getDetails() {
+    return $this->details;
+  }
+  
+  public function __toString() {
+    return "{$this->method} exception [{$this->code}]: {$this->getMessage()} ({$this->details})\n";
+  }
+}
+
+
+?>
---
+++ phpviddler.php
@@ -0,0 +1,437 @@
+<?php
+/*
+	#######################################################
+	#  Viddler API / PHP Wrapper
+	#  By: Colin Devroe | cdevroe@viddler.com
+	#
+	#  Docs: http://wiki.developers.viddler.com/index.php/Phpviddler
+	#
+	#  License(s): Dual licensed under: 
+	#  MIT (MIT-LICENSE.txt)
+    #  GPL (GPL-LICENSE.txt)
+    # 
+    #  Third-party code:
+    #  XML Library by Keith Devens
+	#  xmlparser.php
+	#
+	#  Version 0.4
+	########################################################
+*/
+
+# XML Library, by Keith Devens, version 1.2b
+# http://keithdevens.com/software/phpxml
+include('xmlparser.php');
+
+class Phpviddler {
+
+	var $apiKey;  // To obtain an API key: http://developers.viddler.com/
+	var $viddlerREST = 'http://api.viddler.com/rest/v1/'; // REST URL Version 1.0
+	var $oEmbedREST  = 'http://lab.viddler.com/services/oembed/';
+	var $parser = true; // Use the included XML parser? Default: true.
+	var $debug = false; // Switch for debug mode
+
+/*##########  User functions ########### */
+	
+	/* viddler.users.register
+	/ accepts: $userInfo(array)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.users.register
+	*/
+	function user_register($userInfo=null) {
+		
+		$username = $this->sendRequest('viddler.users.register',$userInfo,'post');
+		
+		return $username;
+	}
+	
+	
+	/* viddler.users.auth
+	/ accepts: $user(string),$pass(string),$getToken(1/optional)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.users.auth
+	*/
+	function user_authenticate($user=null,$pass=null,$getToken=null) {
+		
+		$session = $this->sendRequest('viddler.users.auth','user='.$user.'&password='.$pass.'&get_record_token='.$getToken);
+		
+		return $session;
+	}
+	
+	/* viddler.users.getProfile
+	/ accepts: $user(string)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.users.getProfile
+	*/
+	function user_profile($user=null) {
+		
+		$profile = $this->sendRequest('viddler.users.getProfile','user='.$user);
+		
+		return $profile;
+	}
+	
+	/* viddler.users.setProfile
+	/ requires: POST
+	/ accepts: $profile(array)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.users.setProfile
+	*/
+	function user_setprofile($profile=null) {
+		
+		$newProfile = $this->sendRequest('viddler.users.setProfile',$profile,'post');
+		
+		return $newProfile;
+	}
+	
+	/* viddler.users.setOptions
+	/ requires: POST
+	/ accepts: $options(array)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.users.setOptions
+	*/
+	function user_setoptions($options=null) {
+	
+		$response = $this->sendRequest('viddler.users.setOptions',$options,'post');
+		
+		return $response;
+	}
+	
+/*##########  Video functions ########### */
+
+	/* viddler.videos.upload
+	/ requires: POST
+	/ accepts: $videoInfo(array)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.upload
+	*/
+	function video_upload($videoInfo=null) {
+		
+		$videoDetails = $this->sendRequest('viddler.videos.upload',$videoInfo,'post');
+		
+		return $videoDetails;	
+	}
+	
+	/* viddler.videos.getRecordToken
+	/ accepts: $sessionid(number)
+	/ returns: number | string if error
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getRecordToken
+	/ doc: http://wiki.developers.viddler.com/index.php/Record_With_Webcam_API
+	*/
+	function video_getrecordtoken($sessionid=null) {
+		
+		$token = $this->sendRequest('viddler.videos.getRecordToken','sessionid='.$sessionid);
+		
+		if ($token['error']) {
+			return $token['error']['description'];
+		} else {
+			return $token['record_token'];
+		}
+	}
+
+	/* viddler.videos.getStatus
+	/ accepts: $videoid(number),$sessionid(number/optional)
+	/ returns: array or xml
+	/ Method specific responses {
+	    1: Waiting in encode queue (failed=0)
+		2: Encoding (failed=0)
+		3: Encoding process failed (failed=1)
+		4: Ready (failed=0)
+		5: Deleted (failed=0)
+		6: Wrong priviledges (failed=0)
+	}
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getStatus
+	*/
+	function video_status($videoid=null,$sessionid=null) {
+		
+		$videoStatus = $this->sendRequest('video.status','video_id='.$videoid.'&sessionid='.$sessionid);
+		
+		return $videoStatus;
+	}
+	
+	/* viddler.videos.getDetails
+	/ accepts: $sessionid(number/optional) and $videoid(number)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getDetails
+	*/
+	function video_details($videoid=null,$sessionid=null) {
+		
+		$videoDetails = $this->sendRequest('viddler.videos.getDetails','sessionid='.$sessionid.'&video_id='.$videoid);
+		
+		return $videoDetails;
+	}
+	
+	/* viddler.videos.getDetailsByUrl
+	/ accepts $sessionid(number/optional) and $videourl(string)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getDetailsByUrl
+	*/
+	function video_detailsbyurl($videourl=null,$sessionid=null) {
+	  if($videourl && !strpos($videourl, 'explore')) $videourl = str_replace('viddler.com/', 'viddler.com/explore/', $url);
+	  
+	  $videoDetails = $this->sendRequest('viddler.videos.getDetailsByUrl','sessionid='.$sessionid.'&url='.$videourl);
+	  
+	  return $videoDetails;
+	}
+	
+	/* viddler.videos.setDetails
+	/ accepts: array
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.setDetails
+	*/
+	function video_setdetails($videoDetails=null) {
+	
+		$newVideoDetails = $this->sendRequest('viddler.videos.setDetails',$videoDetails,'post');
+		
+		return $newVideoDetails;
+	}
+	
+	/* viddler.videos.comments.add
+	/ accepts $video_id(string), $text(string), $sessionid
+	*/
+	function video_addcomment($video_id=null, $text=null, $sessionid=null) {
+		$array = array('video_id' => $video_id, 'text' => $text, 'sessionid' => $sessionid);
+		$comment = $this->sendRequest('viddler.videos.comments.add',$array,'post');
+		
+		return $comment;
+	}
+
+
+	/* viddler.videos.getByUser
+	/ accepts: $user(string), $page(number), $per_page(number), $sessionid(number)
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getByUser
+	*/
+	function videos_listbyuser($user=null,$page=null,$per_page=null,$sessionid=null) {
+		
+		$videoList = $this->sendRequest('viddler.videos.getByUser','user='.$user.'&sessionid='.$sessionid.'&page='.$page.'&per_page='.$per_page);
+		
+		return $videoList;
+	}
+	
+	
+	/* viddler.videos.getByTag
+	/ accepts: $tag = string, $page = number, $per_page = number
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getByTag
+	*/
+	function videos_listbytag($tag=null,$page=null,$per_page=null) {
+		
+		$videoList = $this->sendRequest('viddler.videos.getByTag','tag='.$tag.'&page='.$page.'&per_page='.$per_page);
+		
+		return $videoList;
+	}
+	
+	/* viddler.videos.getFeatured
+	/ accepts: none
+	/ returns: array or xml
+	/ doc: http://wiki.developers.viddler.com/index.php/Viddler.videos.getFeatured
+	*/
+	function videos_listfeatured() {
+		
+		$featuredVideos = $this->sendRequest('viddler.videos.getFeatured');
+		
+		return $featuredVideos;
+	} // end videos_listfeatured()
+	
+/*########## Extended Functions ###########
+	Although not available via the API itself yet,
+	these functions can be used to do common
+	tasks. */
+	
+	/* video_addTag()
+	/ accepts: $videoid(number), $sessionid(number), $newtag(string)
+	/ returns: same as video_getdetails();
+	*/
+	function video_addTag($videoid=null,$sessionid=null,$newtag=null) {
+		
+		$videodetails = $this->video_details($videoid,$sessionid);
+		
+		if (is_array($videodetails['video']['tags']['global']) && count($videodetails['video']['tags']['global']) > 1) {
+			for ($i=0;$i<count($videodetails['video']['tags']['global']);$i++) {
+				$videodetails['video']['tags']['global'][$i] = '"'.$videodetails['video']['tags']['global'][$i].'"';
+			}
+			$tags = implode(",",$videodetails['video']['tags']['global']);
+		} else {
+			$tags = $videodetails['video']['tags']['global'];
+		}
+		
+		if ($tags == '') {
+			$tags = $newtag;
+		} else {
+			$tags .= ','.$newtag;
+		}
+				
+		$newvideodetails = $this->video_setdetails(array('video_id'=>$videoid,'sessionid'=>$sessionid,'tags'=>$tags));
+		
+		return $newvideodetails;
+	
+	}
+	
+	/* video_removeTag()
+	/ accepts: $videoid(number), $sessionid(number), $oldtag(string)
+	/ returns: same as video_getdetails();
+	*/
+	function video_removeTag($videoid=null,$sessionid=null,$oldtag=null) {
+		
+		$videodetails = $this->video_details($videoid,$sessionid);
+		
+		if (is_array($videodetails['video']['tags']['global']) && count($videodetails['video']['tags']['global']) > 1) {
+			
+			if (in_array($oldtag,$videodetails['video']['tags']['global'])) {
+				
+				for ($i=0;$i<count($videodetails['video']['tags']['global']);$i++) {
+						if ($videodetails['video']['tags']['global'][$i] == $oldtag) {
+							unset($videodetails['video']['tags']['global'][$i]);
+						} else {
+							$videodetails['video']['tags']['global'][$i] = '"'.$videodetails['video']['tags']['global'][$i].'"';
+						}
+				}
+			
+			} else {
+				return false;
+			}
+			
+			$tags = implode(",",$videodetails['video']['tags']['global']);
+		
+		} elseif ($oldtag == $videodetails['video']['tags']['global']) {
+			$tags = '';
+		} else {
+			return false;
+		}
+				
+		$newvideodetails = $this->video_setdetails(array('video_id'=>$videoid,'sessionid'=>$sessionid,'tags'=>$tags));
+		
+		return $newvideodetails;
+	
+	}
+
+
+
+/*##########  Misc. Functions ########### */
+
+	/* video_getrecordembed()
+	/ accepts: $token(number),$width(number),$height(number)
+	/ returns: HTML
+	*/
+	function video_getRecordEmbed($token=null,$width=449,$height=400) {
+		
+		if (!$token) return false;
+		
+		$html = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="'.$width.'" height="'.$height.'" id="viddler_recorder" align="middle">
+			<param name="allowScriptAccess" value="always" />
+			<param name="allowNetworking" value="all" />
+			<param name="movie" value="http://www.viddler.com/flash/recorder.swf" />
+			<param name="quality" value="high" />
+			<param name="scale" value="noScale">
+			<param name="bgcolor" value="#000000" />
+			<param name="flashvars" value="fake=1&recordToken='.$token.'" />
+			<embed src="http://www.viddler.com/flash/recorder.swf" quality="high" scale="noScale" bgcolor="#000000" allowScriptAccess="always" allowNetworking="all" width="'.$width.'" height="'.$height.'" name="viddler_recorder" flashvars="fake=1&recordToken='.$token.'" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+		</object>';
+		
+		return $html;
+	}
+	
+	/* video_getEmbed()
+	/ accepts: $videoid(string),$type(string,(player or simple),default=player),$options
+	// $width(number),$height(number)
+	/ returns: HTML
+	*/
+	function video_getEmbed($videoid=null,$type='player',$width=437,$height=370) {
+		
+		if (!$videoid) return false;
+		
+		$html = '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="'.$width.'" height="'.$height.'" id="viddlerplayer-'.$videoid.'"><param name="movie" value="http://www.viddler.com/'.$type.'/'.$videoid.'/" /><param name="allowScriptAccess" value="always" /><param name="allowFullScreen" value="true" /><embed src="http://www.viddler.com/'.$type.'/'.$videoid.'/" width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" allowScriptAccess="always" allowFullScreen="true" name="viddlerplayer-'.$videoid.'" ></embed></object>';
+		
+		return $html;
+	}
+	
+	/* video_oEmbed()
+	/ accepts: $url(string), $type(string,(player or simple),default=player),
+	// $ratio(string,(widescreen or fullscreen), default=NULL), $width(number or "full", default=437)
+	// $format(string, (xml, json, html), default=xml)
+	*/
+	function video_oEmbed($url, $type='player', $ratio=NULL, $width=437, $format='xml') {
+	   $old_parser = $this->parser;
+	   $old_viddlerREST = $this->viddlerREST;
+	   
+	   if($format != 'xml') $this->parser = false;
+	   $this->viddlerREST = $this->oEmbedREST;
+	   
+	   if($url && !strpos($url, 'explore')) $url = str_replace('viddler.com/', 'viddler.com/explore/', $url);
+	   
+	   $resp = $this->sendRequest('video_oembed', array('url' => $url, 'type' => $type, 'ratio' => $ratio, 'width' => $width, 'format' => $format));
+	   
+	   $this->parser = $old_parser;
+	   $this->viddlerREST = $old_viddlerREST;
+	   
+	   return $resp;
+	}
+	
+	
+	/* buildArguments()
+	/ accepts: $p(array)
+	/ returns: string
+	*/
+	function buildArguments($p) {
+	
+		foreach ($p as $key => $value) {
+			
+			// Skip these
+			if ($key == 'method' || $key == 'submit' || $key == 'MAX_FILE_SIZE') continue;
+			
+			$args .= $key.'='.urlencode($value).'&';
+			
+		} // end foreach
+		
+		// Chop off last ampersand
+		return substr($args, 0, -1);
+	} // end buildArguments()
+	
+
+	/* sendRequest()
+	/ accepts: $method(string), $args(array), $postmethod(string / post,get)
+	/ returns: array or xml
+	*/
+	function sendRequest($method=null,$args=null,$postmethod='get') {
+	
+		// Convert array to string
+    if (is_array($args) && $method != 'viddler.videos.upload') $args = $this->buildArguments($args);
+		
+		$reqURL = $this->viddlerREST.'?api_key='.$this->apiKey.'&method='.$method;
+		if ($postmethod == 'get') {
+			$reqURL .= '&'.$args;
+		}
+
+		$curl_handle = curl_init();
+		curl_setopt ($curl_handle, CURLOPT_URL, $reqURL);
+		curl_setopt ($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt ($curl_handle, CURLOPT_CONNECTTIMEOUT, 1);
+		curl_setopt ($curl_handle, CURLOPT_HEADER, 0);
+		curl_setopt ($curl_handle, CURLOPT_TIMEOUT, 0);
+		if ($postmethod == 'post') {
+			curl_setopt($curl_handle, CURLOPT_POST, 1);
+			curl_setopt($curl_handle, CURLOPT_POSTFIELDS, $args);
+		}
+		$response = curl_exec($curl_handle);
+		
+		if (!$response)	$response = curl_error($curl_handle);
+		curl_close($curl_handle);
+		
+		// Debug?
+		if ($this->debug) {
+			echo '<pre>';
+				print_r($response);
+			echo '</pre>';
+		}
+		
+		// Return array or XML
+		if ($this->parser) {
+			return XML_unserialize($response);
+		} else {
+			return $response;
+		}
+        
+	} // End sentRequest();
+
+} // end phpviddler
+
+?>
