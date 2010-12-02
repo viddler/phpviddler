1943cf7cbbbbebecbd16adf0183cae90460d551e
Colin Devroe <colin@cdevroe.com>
2010-12-02 16:19:12 -0500

    First release of PHPViddler 2.0

--- README.markdown
+++ README.markdown
@@ -1,45 +1,31 @@
-phpViddler API Library
+phpViddler API v2 Library
 ======================
 
-A PHP Wrapper class for Viddler’s REST Interface. Wraps each method into their own functions and includes an open source XML Parser.
+A PHP class to wrap Viddler's API version 2. To access version 1 of PHPViddler please see [the v1.0 tag on GitHub](https://github.com/viddler/phpviddler/tree/v1.0).
 
-For more information, visit [our developer site](http://developers.viddler.com/projects/api-wrappers/phpviddler/)
+For more information, visit [our developer site](http://developers.viddler.com/)
 
 Installation Instructions
 -------------------------
-phpViddler is meant to be installed into most any PHP-powered web site or application.
+PHPViddler is fully OOP. Like snOOP dogg. Ok, not like him.
 
-1. Download the most recent version.
-2. Upload phpviddler.php and xmlparser.php to your server.
-3. Include phpviddler.php in your web site or application.
-4. Initiate Viddler class $v = new Phpviddler();
-5. Setup API Key variable $v->apikey=KEY_HERE
+1. [Download the most recent version](https://github.com/viddler/phpviddler/tree/v2.0).
+2. Upload phpviddler.php
+3. Include phpviddler.php
+4. Initiate Viddler class like this $v = new Viddler_V2('YOUR API KEY HERE');
 
 Usage
 -----
-    $v = new Phpviddler('your api key');
+    $v = new Viddler_V2('Your API Key');
     
-    // Find videos by user
-    $videos = $v->videos_listbyuser('kyleslat');
-    foreach($videos['video_list']['video'] as $video) {
-      echo $v->video_getEmbed($video['id']);
+    // Example find videos by user
+    $videos = $v->viddler_videos_getByUser('viddlerdevtest');
+    foreach($videos['list_result']['video_list'] as $video) {
+      print_r($video);
     }
 
-Included in PHPviddler is an /examples/ directory with a few code examples.
-
-For more tutorials check the [Viddler Development Blog](http://developers.viddler.com/category/tutorials/phpviddler/)
+Included in PHPviddler is an /examples/ directory with a few code examples. These have been updated to use version 2 of our API.
 
 Licensing
 ---------
-phpViddler is dual-licensed under the MIT and GPL licenses. The details of these licenses are included with the zip file.
-
-Third-party software included
------------------------------
-* [XML Library](http://keithdevens.com/software/phpxml) by Keith Devens, version 1.2b (optional)
-
-At present we include the XML Library as a means to parse the REST API’s responses, though it is configurable to turn off for applications that already have an XML Parser. See readme for details.
-
-PHP 5
------
-Included is a class `Php5viddler` which will raise exceptions (supported in PHP 5) if an error is returned from the API.  If you use `Phpviddler`, you need to check the response to see if there is an error node.
-
+phpViddler is dual-licensed under the MIT License. The details of this can be found MITlicense.txt
--- examples/getExamples.php
+++ examples/getExamples.php
@@ -1,15 +1,13 @@
 <?php
 
-// These examples are in PHP 5.
-
-include('../php5viddler.php');
-$v = new Phpviddler(''); // Get an API key by going to You > Profile & API on Viddler.
+include('../phpviddler2.php');
+$v = new Viddler_V2('01171d24e48b43444556524f45b3'); // Get an API key by going to You > Profile & API on Viddler.
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PHPViddler GET Examples</title>
+		<title>PHPViddler API v2 GET Examples</title>
 		
 	</head>
 	
@@ -18,42 +16,62 @@ $v = new Phpviddler(''); // Get an API key by going to You > Profile & API on Vi
 		<p>A few simple examples of retrieving some videos from Viddler's API using PHPViddler.</p>
 		
 		
-		<h2><a href="http://developers.viddler.com/documentation/api/method-videos-getbyuser/">viddler.videos.getByUser</a></h2>
+		<h2><a href="http://developers.viddler.com/documentation/api-v2/">viddler.videos.getByUser</a></h2>
 		<p>Some videos based on a user - viddlerdevtest (a Viddler account we use for testing)</p>
 		<p><?php 
 		
 		// Get videos (page 1, videos 5)
-		$videos = $v->videos_listbyuser('viddlerdevtest',1,5);
+		$videos = $v->viddler_videos_getByUser(array('user'=>'viddlerdevtest','per_page'=>5,'page'=>1));
+		
+		/* Debug only 
+		echo '<pre>';
+		print_r($videos);
+		echo '</pre>';
+		*/
 		
 		// Loop through videos showing just the thumbnail
 		// with a link to the video itself.
-		foreach($videos['video_list']['video'] as $video) {
+		foreach($videos['list_result']['video_list'] as $video) {
 			echo '<a href="'.$video['url'].'"><img src="'.$video['thumbnail_url'].'" alt="thumbnail" width="60" /></a> ';
 		}
 		?></p>
 		
 		
-		<h2><a href="http://developers.viddler.com/documentation/api/method-videos-search/">viddler.videos.search</a></h2>
+		<h2><a href="http://developers.viddler.com/documentation/api-v2/">viddler.videos.search</a></h2>
 		<p>Search all of Viddler's public videos for "iPhone".</p>
 		<p><?php 
 		
 		// Search videos: type=allvideos, query=iphone, videos 5
-		$videos = $v->video_search(array('type'=>'allvideos','query'=>'iphone','per_page'=>5));
+		$videos = $v->viddler_videos_search(array('type'=>'allvideos','query'=>'iphone','per_page'=>5));
+		
+
+		/* Debug only 
+		echo '<pre>';
+		print_r($videos);
+		echo '</pre>';
+		*/
+
 		
 		// Loop through videos showing just the thumbnail
 		// with a link to the video itself.
-		foreach($videos['video_list']['video'] as $video) {
+		foreach($videos['list_result']['video_list'] as $video) {
 			echo '<a href="'.$video['url'].'"><img src="'.$video['thumbnail_url'].'" alt="thumbnail" width="60" /></a> ';
 		}
 		?></p>
 		
 		
-		<h2><a href="http://developers.viddler.com/documentation/api/method-users-getprofile/">viddler.users.getProfile</a></h2>
+		<h2><a href="http://developers.viddler.com/documentation/api-v2/">viddler.users.getProfile</a></h2>
 		<p>Show a user's profile.</p>
 		<?php 
 		
 		// Search videos: type=allvideos, query=iphone, videos 5
-		$userInfo = $v->user_profile('viddlerdevtest');
+		$userInfo = $v->viddler_users_getProfile(array('user'=>'cdevroe'));
+		
+		/*
+		echo '<pre>';
+		print_r($userInfo);
+		echo '</pre>';
+		*/
 		
 		echo '<p><strong>Username:</strong> '.$userInfo['user']['username'].'</p>';
 		echo '<p><strong>Avatar:</strong> <img src="'.$userInfo['user']['avatar'].'" /></p>';
@@ -62,16 +80,7 @@ $v = new Phpviddler(''); // Get an API key by going to You > Profile & API on Vi
 		?>
 		<p>Note: If Avatar is blank you should use default. More profile variables are available, review doc.</p>
 		
-		<h2>Creating a Viddler short URL</h2>
-		<p>Create a short URL using http://go.viddler.com/ using the following URL as an example:<br />URL: <a href="http://www.viddler.com/explore/cdevroe/videos/133/">http://www.viddler.com/explore/cdevroe/videos/133/</a></p>
-		
-		<?php $shortURL = $v->video_go('http://www.viddler.com/explore/cdevroe/videos/133/');?>
-		<p>Short URL: <a href="<?=$shortURL;?>"><?=$shortURL;?></a></p>
-		
 		<p>Please feel free to add more examples. Most GET requests return similar results though.</p>
-		
-		
-		
 		
 	</body>
 </html>
