69708a2a7d02d4e9180077d63a2e29491ce46049
Jeff Johns <phpfunk@gmail.com>
2011-02-24 20:11:18 -0500

    Removed CURLOPT_CONNECTTIMEOUT, causing connection issues.

--- phpviddler.php
+++ phpviddler.php
@@ -134,7 +134,6 @@ class Viddler_V2 {
     $ch = curl_init();
     curl_setopt ($ch, CURLOPT_URL, $url);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 1);
     curl_setopt ($ch, CURLOPT_HEADER, 0);
     curl_setopt ($ch, CURLOPT_TIMEOUT, 0);
     curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
